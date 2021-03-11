16391cd943cc54b85892f7d2baf46f3374276c61
Reggie <cushingreggie@gmail.com>
2021-03-11 11:37:12 +0100

    switch to actor abstraction

--- bucket.js
+++ bucket.js
@@ -1,37 +1,27 @@
 const cmdArgs = require('command-line-args')
-const secureAmqp = require('../cllibsecureamqp')
 
 const cmdOptions = [
-	{ name: 'send', alias: 's', type: String},
-	{ name: 'config', alias: 'c', type: String}
+	{ name: 'config', alias: 'c', type: String},
+	{ name: 'debug', type: Boolean}
 ]
 const options = cmdArgs(cmdOptions)
+
+const Actor = (options.debug) ?  require('../cllibsecureamqp').Actor : require('secureamqp').Actor
+
 options.config = options.config || "./config"
 const config = require(options.config)
 const toAddress = options.send
 
 async function main() {
-	await secureAmqp.init(config)
-	const myAddress = secureAmqp.getMyAddress()
-	console.log("Actor address: ", myAddress)
-	console.log("Actor keys: ", secureAmqp.keys())
-
-	secureAmqp.registerFunction('.f.send', null, function(req, res) {
+	const bucket = new Actor(config)
+	await bucket.boot()
+	bucket.createAbility('send', false, function(req, res) {
 		console.log("Copy send request ", req.msg)
 		// check token
 		// check auditor ids
 		// open vpn
 		// start data services
-		res.send("OK")
-	})
-
-	secureAmqp.registerFunction('.f.receive', null, function(req, res) {
-		console.log("Copy receive request ", req.msg)
-		// check token
-		// check auditor ids
-		// open vpn
-		// start data services
-		res.send("OK")
+		res.send("OK", 200)
 	})
 }
 
