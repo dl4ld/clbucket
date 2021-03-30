29c004eca939573300d421e9856d78684a26a445
Reggie <cushingreggie@gmail.com>
2021-03-30 12:33:23 +0200

    switch to actor abstraction

--- bucket.js
+++ bucket.js
@@ -10,11 +10,27 @@ const Actor = (options.debug) ?  require('../cllibsecureamqp').Actor : require('
 
 options.config = options.config || "./config"
 const config = require(options.config)
-const toAddress = options.send
+const toAddress = options.senda
+const data = [1,2,3,4,5,6,7,8,9,0]
+const manifest = {
+	name: 'myData',
+	type: Array,
+	iat: new Date().toISOString(),
+	hash: "deadbeef",
+	authActors: ["YAkJunLY0Pv3AjZHD++blx5PKOYydceseKmejkQqCGA="]
+}
 
 async function main() {
 	const bucket = new Actor(config)
 	await bucket.boot()
+	bucket.exposeAsset('myData', manifest, data, function(req) {
+		// return true or flase to allow to serve data
+		return true
+	})
+	setInterval(() => {
+		data[0] += 1
+		bucket.updateData('myData', manifest, data)
+	}, 3000)
 	bucket.createAbility('send', false, function(req, res) {
 		console.log("Copy send request ", req.msg)
 		// check token
